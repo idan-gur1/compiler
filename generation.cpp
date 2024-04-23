64b8374e194f788d1fd2f643b2c2ac9d91009d0c
idang <idangur100@gmail.com>
2024-04-24 02:54:55 +0300

    finished expression to register conversion. not tested

--- generation.cpp
+++ generation.cpp
@@ -19,46 +19,185 @@ void Generator::generateProgram() {
     }
 }
 
-std::string Generator::convertUniExprToRegister(UniExprP expr, std::string reg) {
+std::string Generator::movTo64BitReg(const std::string &reg, const std::string &val, int size) {
+    std::string movType = "mov ";
+
+    if (size < bit64RegSize) {
+        movType = "movsx ";
+    }
+
+    return movType + reg + ", " + val;
+}
+
+std::string Generator::getStackAddr(const VariableStackData &var) {
+    if (var.stackPos > 0) {
+        return "bp + " + std::to_string(var.stackPos);
+    }
+
+    return "bp - " + std::to_string(std::abs(var.stackPos));
+}
+
+std::string Generator::getSubscriptableStackPosition(SubscriptableVariableValP subVar, const std::string &freeReg) {
+    VariableStackData varData = this->variableStack[subVar->var.name].top();
+
+    std::string varBaseAddr = getStackAddr(varData);
+
+    if (subVar->var.ptrType) {
+        this->programOut << "mov " << freeReg << ", QWORD [" << varBaseAddr << "]\n";
+        varBaseAddr = freeReg;
+    }
+
+    std::string offset = "rcx";
+
+    if (auto imIntIndex = dynamic_cast<ImIntValP>(subVar->index)) {
+        offset = imIntIndex->value;
+    } else {
+        convertUniExprToRegister(subVar->index, "rcx");
+    }
+
+    return "[" + varBaseAddr + " + " + std::to_string(varData.varSize) + " * " + offset + "]";
+}
+
+void Generator::convertUniExprToRegister(UniExprP expr, const std::string &reg) {
     if (auto imInt = dynamic_cast<ImIntValP>(expr)) {
-        this->programOut << "mov " << Generator::getRegisterBySize(8, reg) << ", " << imInt->value;
+        this->programOut << "mov " << reg << ", " << imInt->value << "\n";
     } else if (auto temp = dynamic_cast<UniTempP>(expr)) {
-        this->programOut << "mov " << Generator::getRegisterBySize(this->tempSizes[temp->id], reg) << ", "
-                         << this->sizeIdentifiers[this->tempSizes[temp->id]] << "[bp - " << temp->id << "]";
+        this->programOut << "mov " << reg << ", QWORD [bp - " << temp->id << "]\n";
     } else if (auto subVar = dynamic_cast<SubscriptableVariableValP>(expr)) {
+        VariableStackData varData = this->variableStack[subVar->var.name].top();
 
+        std::string varAddr = this->sizeIdentifiers[varData.varSize] + " " +
+                              getSubscriptableStackPosition(subVar, reg);
+
+        this->programOut << movTo64BitReg(reg, varAddr, varData.varSize) << "\n";
     } else if (auto var = dynamic_cast<VariableValP>(expr)) {
+        VariableStackData varData = this->variableStack[var->var.name].top();
 
+        std::string varAddr = this->sizeIdentifiers[varData.varSize] +
+                              " [" + getStackAddr(varData) + "]";
+
+        this->programOut << movTo64BitReg(reg, varAddr, varData.varSize) << "\n";
     } else if (auto logicalNot = dynamic_cast<LogicalNotExprP>(expr)) {
-
+        convertUniExprToRegister(logicalNot->expr, reg);
+        this->programOut << "test " << reg << ", " << reg << "\n";
+        this->programOut << "setz dl\n";
+        this->programOut << "movzx " << reg << " dl\n";
     } else if (auto numericNeg = dynamic_cast<NumericNegExprP>(expr)) {
-
+        convertUniExprToRegister(logicalNot->expr, reg);
+        this->programOut << "neg " << reg << "\n";
     } else if (auto functionCall = dynamic_cast<FunctionCallExprP>(expr)) {
-
+        // WON'T REACH HERE
     }
 }
 
-std::string Generator::convertAddrExprToRegister(AddrExprP expr, std::string reg) {
-    return std::string();
+void Generator::convertAddrExprToRegister(AddrExprP expr, const std::string &reg) {
+    if (auto subVar = dynamic_cast<SubscriptableVariableValP>(expr->addressable)) {
+        this->programOut << "lea " << reg << ", " << getSubscriptableStackPosition(subVar, reg) << "\n";
+    } else {
+        Variable var = expr->addressable->var;
+        VariableStackData varData = this->variableStack[var.name].top();
+
+        std::string varBaseAddr = getStackAddr(varData);
+
+        if (var.ptrType) {
+            this->programOut << "mov " << reg << ", QWORD [" << varBaseAddr << "]\n";
+        } else {
+            this->programOut << "lea " << reg << ", [" << varBaseAddr << "]\n";
+        }
+    }
 }
 
-std::string Generator::convertBinaryExprToRegister(BinaryExprP expr, std::string reg) {
-    return std::string();
+void Generator::convertBinaryExprToRegister(BinaryExprP expr) {
+    convertUniExprToRegister(expr->left, "rax");
+    convertUniExprToRegister(expr->left, "rbx");
+
+    if (this->BinaryExprToAsmStrSteps.contains(expr->op)) {
+        this->programOut << this->BinaryExprToAsmStrSteps[expr->op];
+    } else if (expr->op == ExprOperator::logicalOr) {
+        std::string orTrue = "orTrue" + std::to_string(++this->labelCount);
+        std::string orFalse = "orFalse" + std::to_string(++this->labelCount);
+        std::string orEnd = "orEnd" + std::to_string(++this->labelCount);
+
+        this->programOut << "test rax, rax\n" <<
+                         "jnz " << orTrue << "\n" <<
+                         "test rbx, rbx\n" <<
+                         "jz " << orFalse << "\n" <<
+                         orTrue << ":\n" <<
+                         "mov rax, 1\n"
+                         "jmp " << orEnd << "\n" <<
+                         orFalse << ":\n" <<
+                         "mov rax, 0\n" <<
+                         orEnd << ":\n";
+    } else if (expr->op == ExprOperator::logicalAnd) {
+        std::string andFalse = "andFalse" + std::to_string(++this->labelCount);
+        std::string andEnd = "andEnd" + std::to_string(++this->labelCount);
+
+        this->programOut << "test rax, rax\n" <<
+                         "jz " << andFalse << "\n" <<
+                         "test rbx, rbx\n" <<
+                         "jz " << andFalse << "\n" <<
+                         "mov rax, 1\n"
+                         "jmp " << andEnd << "\n" <<
+                         andFalse << ":\n" <<
+                         "mov rax, 0\n" <<
+                         andEnd << ":\n";
+    }
+
+    // REPLACED BY UNORDERED MAP
+    /*if (expr->op == ExprOperator::add) {
+        this->programOut << "add rax, rbx\n";
+    } else if (expr->op == ExprOperator::sub) {
+        this->programOut << "sub rax, rbx\n";
+    } else if (expr->op == ExprOperator::mult) {
+        this->programOut << "imul rbx\n";
+    } else if (expr->op == ExprOperator::div) {
+        this->programOut << "cqo\n"
+                            "idiv rbx\n";
+    } else if (expr->op == ExprOperator::mod) {
+        this->programOut << "cqo\n"
+                            "idiv rbx\n"
+                            "mov rax, rdx\n";
+    } else if (expr->op == ExprOperator::equals) {
+        this->programOut << "xor rax, rbx\n"
+                            "setz al\n"
+                            "movzx rax, al\n";
+    } else if (expr->op == ExprOperator::notEquals) {
+        this->programOut << "xor rax, rbx\n"
+                            "setnz al\n"
+                            "movzx rax, al\n";
+    } else if (expr->op == ExprOperator::biggerThan) {
+        this->programOut << "cmp rax, rbx\n"
+                            "setg al\n"
+                            "movzx rax, al\n";
+    } else if (expr->op == ExprOperator::biggerThanEquals) {
+        this->programOut << "cmp rax, rbx\n"
+                            "setge al\n"
+                            "movzx rax, al\n";
+    } else if (expr->op == ExprOperator::lessThan) {
+        this->programOut << "cmp rax, rbx\n"
+                            "setl al\n"
+                            "movzx rax, al\n";
+    } else if (expr->op == ExprOperator::lessThanEquals) {
+        this->programOut << "cmp rax, rbx\n"
+                            "setle al\n"
+                            "movzx rax, al\n";
+    }*/
 }
 
-std::string Generator::convertTAExprToRegister(ThreeAddressExprP expr, const std::string &reg) {
+void Generator::convertTAExprToRegister(ThreeAddressExprP expr) {
     if (auto uni = dynamic_cast<UniExprP>(expr)) {
-        return convertUniExprToRegister(uni, reg);
+        convertUniExprToRegister(uni, "rax");
     } else if (auto addr = dynamic_cast<AddrExprP>(expr)) {
-        return convertAddrExprToRegister(addr, reg);
+        convertAddrExprToRegister(addr, "rax");
     } else if (auto binary = dynamic_cast<BinaryExprP>(expr)) {
-        return convertBinaryExprToRegister(binary, reg);
+        convertBinaryExprToRegister(binary);
     }
 
-    return "";
+    //return "";
 }
 
 void Generator::convertTAStmtToAsm(ThreeAddressStmtP taStmt) {
+
     if (auto tempAssignment = dynamic_cast<TempAssignmentTAStmtP>(taStmt)) {
         convertTempAssignmentToAsm(tempAssignment);
     } else if (auto varAssignment = dynamic_cast<VarAssignmentTAStmtP>(taStmt)) {
@@ -89,7 +228,7 @@ void Generator::convertTAStmtToAsm(ThreeAddressStmtP taStmt) {
 }
 
 void Generator::convertTempAssignmentToAsm(TempAssignmentTAStmtP stmt) {
-
+    convertTAExprToRegister(stmt->expr);
 }
 
 void Generator::convertVarAssignmentToAsm(VarAssignmentTAStmtP stmt) {
--- generation.h
+++ generation.h
@@ -6,38 +6,97 @@
 #define COMPILER_GENERATION_H
 
 #include <utility>
+#include <stack>
 
 #include "intermediateCodeGenerator.h"
 
+
+class VariableStackData {
+public:
+    int stackPos;
+    int varSize;
+
+    VariableStackData(int stackPos, int varSize) : stackPos(stackPos), varSize(varSize) {
+
+    }
+};
+
 class Generator {
 public:
-    explicit Generator(ILGenerator *ilGenerator, std::string outFileName) : outFileName(std::move(outFileName)) {
+    Generator(ILGenerator *ilGenerator, std::string outFileName) : outFileName(std::move(outFileName)) {
         this->ilGenerator = ilGenerator;
+
+        typeSizes = {
+                {VariableType::intType,  4},
+                {VariableType::charType, 1},
+        };
+
+        sizeIdentifiers = {
+                {8, "QWORD"},
+                {4, "DWORD"},
+                {2, "WORD"},
+                {1, "BYTE"},
+        };
+
+        BinaryExprToAsmStrSteps = {
+                {ExprOperator::add,              "add rax, rbx\n"},
+
+                {ExprOperator::sub,              "sub rax, rbx\n"},
+
+                {ExprOperator::mult,             "imul rbx\n"},
+
+                {ExprOperator::div,              "cqo\n"
+                                                 "idiv rbx\n"},
+
+                {ExprOperator::mod,              "cqo\n"
+                                                 "idiv rbx\n"
+                                                 "mov rax, rdx\n"},
+
+                {ExprOperator::equals,           "xor rax, rbx\n"
+                                                 "setz al\n"
+                                                 "movzx rax, al\n"},
+
+                {ExprOperator::notEquals,        "xor rax, rbx\n"
+                                                 "setnz al\n"
+                                                 "movzx rax, al\n"},
+
+                {ExprOperator::biggerThan,       "cmp rax, rbx\n"
+                                                 "setg al\n"
+                                                 "movzx rax, al\n"},
+
+                {ExprOperator::biggerThanEquals, "cmp rax, rbx\n"
+                                                 "setge al\n"
+                                                 "movzx rax, al\n"},
+
+                {ExprOperator::lessThan,         "cmp rax, rbx\n"
+                                                 "setl al\n"
+                                                 "movzx rax, al\n"},
+
+                {ExprOperator::lessThanEquals,   "cmp rax, rbx\n"
+                                                 "setle al\n"
+                                                 "movzx rax, al\n"}
+        };
     }
 
     void generateProgram();
 
 private:
-    const int ptrSize = 8;
-    std::unordered_map<VariableType, int> typeSizes = {
-            {VariableType::intType, 4},
-            {VariableType::charType, 1},
-    };
-    std::unordered_map<int, std::string> sizeIdentifiers = {
-            {8, "QWORD"},
-            {4, "DWORD"},
-            {2, "WORD"},
-            {1, "BYTE"},
-    };
-    int *tempSizes;
+    static const int bit64RegSize = 8;
+    static const int ptrSize = 8;
 
-    std::string convertUniExprToRegister(UniExprP, std::string);
+    static std::string getStackAddr(const VariableStackData &);
 
-    std::string convertAddrExprToRegister(AddrExprP, std::string);
+    static std::string movTo64BitReg(const std::string &, const std::string &, int);
 
-    std::string convertBinaryExprToRegister(BinaryExprP, std::string);
+    std::string getSubscriptableStackPosition(SubscriptableVariableValP subVar, const std::string &reg);
 
-    std::string convertTAExprToRegister(ThreeAddressExprP, const std::string &);
+    void convertUniExprToRegister(UniExprP, const std::string &);
+
+    void convertAddrExprToRegister(AddrExprP, const std::string &);
+
+    void convertBinaryExprToRegister(BinaryExprP);
+
+    void convertTAExprToRegister(ThreeAddressExprP);
 
     void convertTAStmtToAsm(ThreeAddressStmtP);
 
@@ -69,9 +128,15 @@ private:
 
     static std::string getRegisterBySize(int, const std::string &);
 
+    std::unordered_map<VariableType, int> typeSizes;
+    std::unordered_map<int, std::string> sizeIdentifiers;
+    std::unordered_map<ExprOperator, std::string> BinaryExprToAsmStrSteps;
+    std::unordered_map<std::string, std::stack<VariableStackData>> variableStack;
+
     ILGenerator *ilGenerator;
     std::string outFileName;
     std::stringstream programOut;
+    int labelCount = 0;
 };
 
 #endif //COMPILER_GENERATION_H
